bf4077e06516d2a731545114fffff0c2d7b3ac0e
ErneDainesi <edainesi@fi.uba.ar>
2024-10-09 03:04:04 -0300

    add wip screens

---
+++ app/src/main/java/com/example/parcial_tp3_grupo_1/screens/signin/SignInScreen.kt
@@ -0,0 +1,16 @@
+package com.example.parcial_tp3_grupo_1.screens.signin
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.example.parcial_tp3_grupo_1.navigation.MainNavActions
+
+@Composable
+fun SignInScreen(
+    navigationActions: MainNavActions,
+) {
+    Column {
+        Text(text = "SignIn WIP")
+    }
+}
---
+++ app/src/main/java/com/example/parcial_tp3_grupo_1/screens/signup/SignUpRoute.kt
@@ -0,0 +1,11 @@
+package com.example.parcial_tp3_grupo_1.screens.signup
+
+import androidx.compose.runtime.Composable
+import com.example.parcial_tp3_grupo_1.navigation.MainNavActions
+
+@Composable
+fun SignUpRoute(
+    navigationActions: MainNavActions,
+) {
+    SignUpScreen(navigationActions)
+}
