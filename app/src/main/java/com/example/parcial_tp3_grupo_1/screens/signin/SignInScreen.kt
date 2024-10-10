6f2dd5db91b387f4d04d89512cae95457a171e46
ErneDainesi <edainesi@fi.uba.ar>
2024-10-10 17:57:27 -0300

    finish basic login screen

--- app/src/main/java/com/example/parcial_tp3_grupo_1/screens/signin/SignInScreen.kt
+++ app/src/main/java/com/example/parcial_tp3_grupo_1/screens/signin/SignInScreen.kt
@@ -1,24 +1,37 @@
 package com.example.parcial_tp3_grupo_1.screens.signin
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import com.example.parcial_tp3_grupo_1.R
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.parcial_tp3_grupo_1.navigation.MainNavActions
@@ -28,7 +41,29 @@ import com.example.parcial_tp3_grupo_1.ui.components.BasicButton
 fun SignInScreen(
     navigationActions: MainNavActions,
 ) {
-    Column (modifier = Modifier.padding(10.dp)) {
+    var showPassword by remember {
+        mutableStateOf(value = false)
+    }
+    var password by remember {
+        mutableStateOf(value = "")
+    }
+    var email by remember {
+        mutableStateOf(value = "")
+    }
+    Column (
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+    ) {
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(painter = painterResource(R.drawable.carrot), contentDescription = "Carrot icon")
+        }
+        Spacer(modifier = Modifier.height(50.dp))
         Row {
             Column {
                 Text(
@@ -59,15 +94,21 @@ fun SignInScreen(
                     )
                 )
                 TextField(
-                    value = "Hola",
-                    onValueChange = {},
+                    value = email,
+                    onValueChange = { newEmail ->
+                        email = newEmail
+                    },
                     textStyle = TextStyle(
                         fontSize = 18.sp
                     ),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
                     colors = TextFieldDefaults.colors(
                         focusedContainerColor = Color.Transparent,
                         unfocusedContainerColor = Color.Transparent
-                    )
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(0.dp)
                 )
                 Spacer(modifier = Modifier.height(10.dp))
                 Text(
@@ -78,18 +119,33 @@ fun SignInScreen(
                     )
                 )
                 TextField(
-                    value = "hola",
-                    onValueChange = {},
-                    visualTransformation = PasswordVisualTransformation(),
+                    value = password,
+                    onValueChange = { newPassword ->
+                        password = newPassword
+                    },
+                    visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                     textStyle = TextStyle(
                         fontSize = 18.sp
                     ),
                     colors = TextFieldDefaults.colors(
                         focusedContainerColor = Color.Transparent,
                         unfocusedContainerColor = Color.Transparent
-                    )
+                    ),
+                    modifier = Modifier.fillMaxWidth(),
+                    trailingIcon = {
+                        IconButton(onClick = { showPassword = !showPassword }) {
+                            Icon(
+                                painter = painterResource(R.drawable.hidepassword),
+                                contentDescription = "Hide password icon",
+                            )
+                        }
+                    }
                 )
-                TextButton(onClick = {}) {
+                TextButton(
+                    onClick = {},
+                    modifier = Modifier.align(Alignment.End)
+                ) {
                     Text(
                         text = "Forgot Password?",
                         color = Color.Black
@@ -97,8 +153,12 @@ fun SignInScreen(
                 }
             }
         }
-        Row {
-            Column {
+        Row (
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 BasicButton(text = "Sign In", onClick = {})
                 Row {
                     TextButton(onClick = {}) {
--- app/src/main/java/com/example/parcial_tp3_grupo_1/ui/components/BasicButton.kt
+++ app/src/main/java/com/example/parcial_tp3_grupo_1/ui/components/BasicButton.kt
@@ -1,11 +1,13 @@
 package com.example.parcial_tp3_grupo_1.ui.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -21,7 +23,8 @@ fun BasicButton(
         colors = ButtonDefaults.buttonColors(
             containerColor = Color(0xFF53B175)
         ),
-        shape = RoundedCornerShape(10.dp)
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier.fillMaxWidth()
     ) {
         Text(
             text = text,
