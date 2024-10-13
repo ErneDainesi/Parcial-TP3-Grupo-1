8fbf2736e0bf10bce94826e233fa2ce7ce7d48fc
gaspar-costa-calvo <gaspar.c.calvo@gmail.com>
2024-10-13 15:16:43 -0300

    Se implento la SearchBar (falta la logica de filtrado), se implemento el grid de Categorias de la ExploreScreen

--- app/src/main/java/com/example/parcial_tp3_grupo_1/screens/explore/ExploreScreen.kt
+++ app/src/main/java/com/example/parcial_tp3_grupo_1/screens/explore/ExploreScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowColumnScopeInstance.align
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -26,7 +25,6 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarColors
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,15 +33,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.BiasAbsoluteAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.parcial_tp3_grupo_1.R
@@ -66,7 +60,7 @@ val exploreItems = listOf(
     ExploreItem( "Beverages", R.drawable.explore_beverages,  Color(183, 223, 245, 26)),
 )
 
-@Preview
+//@Preview
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ExploreScreen(navigationActions: MainNavActions) {
