b1abbe93d4d6de6e3f3434a8a8630731307b3bd0
ngvior18 <ngvior@gmail.com>
2024-10-13 19:46:23 -0300

    refactor in signInViewModel

--- app/src/main/java/com/example/parcial_tp3_grupo_1/screens/signin/SignInViewModel.kt
+++ app/src/main/java/com/example/parcial_tp3_grupo_1/screens/signin/SignInViewModel.kt
@@ -6,7 +6,7 @@ import com.example.parcial_tp3_grupo_1.shared.FakeStoreService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class SignInViewModel: ViewModel() {
+class SignInViewModel : ViewModel() {
     suspend fun checkLogin(email: String, password: String): Boolean {
         return withContext(Dispatchers.IO) {
             val service = FakeStoreService(FakeStoreHelper())
